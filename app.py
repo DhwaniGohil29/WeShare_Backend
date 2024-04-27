81a82fdc9f1d722983c8f38fe4bbdb0543860032
Siddhi Shah <shahsiddhi777@gmail.com>
2024-04-27 21:43:33 +0530

    Update app.py to run on port 5000 for deployment

--- app.py
+++ app.py
@@ -76,4 +76,4 @@ def login():
     return jsonify({'message': 'Login successful', 'access_token': access_token}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,port=5000)
